0bfaf0579073707820dcf0e5b4ea096c9de821b8
Raven G. Duran <me@imraven.com>
2014-05-23 21:00:46 +0800

    Added day 16

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 	$("#scrolldown").click(function(){
 		//$('#day2').ScrollTo();
-		$('#theday15').animatescroll({scrollSpeed:2000,easing:'easeOutBack'});
+		$('#theday16').animatescroll({scrollSpeed:2000,easing:'easeOutBack'});
 	});
 
 	$("html").niceScroll();
